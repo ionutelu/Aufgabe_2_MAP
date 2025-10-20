f0efbea0a82df6a7f82276ca1a84acf8d4891a8c
ionut <ionutpuiu010405@gmail.com>
2025-10-20 18:09:54 +0300

    Updated the constructor and added 2 helper method to sort the numbers in an ascending order

--- src/Numerator.java
+++ src/Numerator.java
@@ -1,8 +1,48 @@
 public class Numerator {
     private int[] num;
     public Numerator(int[] num) {
-        this.num = num;
+        this.num = num.clone();
+        mergeSort(this.num, 0, this.num.length - 1);
     }
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            // Recursively sort both halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data
+        for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+
     public int[] getNum() {
         return num;
     }
