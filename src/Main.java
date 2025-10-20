b8571a010712dc2eaa30bcf1c75fe7774f6fcae9
ionut <ionutpuiu010405@gmail.com>
2025-10-20 18:24:44 +0300

    Tested the methods in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] numbers = new int[100];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+        Numerator numerator = new Numerator(numbers);
+        System.out.println(numerator.biggestNumber());
+        System.out.println(numerator.smallestNumber());
+        System.out.println(numerator.smallestSum(17));
+        System.out.println(numerator.biggestSum(13));
     }
 }
