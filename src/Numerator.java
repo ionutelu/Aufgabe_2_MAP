d6ac14a5bf05348408cb8743686c25eea187b51a
ionut <ionutpuiu010405@gmail.com>
2025-10-20 18:01:53 +0300

    Created Numerator class, this class will help solve the exercise

---
+++ src/Numerator.java
@@ -0,0 +1,13 @@
+public class Numerator {
+    private int[] num;
+    public Numerator(int[] num) {
+        this.num = num;
+    }
+    public int[] getNum() {
+        return num;
+    }
+    public void setNum(int[] num) {
+        this.num = num;
+    }
+
+}
